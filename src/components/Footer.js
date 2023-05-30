d62c60181be08ca4f57312ec39a35800baf2410e
deeppankaj <you@example.com>
2023-05-30 11:20:56 -0500

    off

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import {BsEnvelope,BsTelephone } from "react-icons/bs";
+import { FiLinkedin ,FiTwitter,FiInstagram,FiFacebook,FiHeart ,FiMapPin,FiChevronRight }from 'react-icons/fi'
 
 const Footer = () => {
   return (
@@ -9,51 +12,127 @@ const Footer = () => {
             <a href="#" className="logo-footer">
               <img src="../assets/images/logo-light.png" height="22" alt="" />
             </a>
-            <p className="mt-4 me-xl-5">Great doctor if you need your family member to get effective immediate assistance, emergency treatment or a simple consultation.</p>
+            <p className="mt-4 me-xl-5">
+              Great doctor if you need your family member to get effective
+              immediate assistance, emergency treatment or a simple
+              consultation.
+            </p>
           </div>
           <div className="col-xl-7 col-lg-8 col-md-12">
             <div className="row">
               <div className="col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
                 <h5 className="footer-head">Company</h5>
                 <ul className="list-unstyled footer-list mt-4">
-                  <li><a href="aboutus.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> About us</a></li>
-                  <li><a href="departments.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Services</a></li>
-                  <li><a href="doctor-team-two.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Team</a></li>
-                  <li><a href="blog-detail.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Project</a></li>
-                  <li><a href="blogs.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Blog</a></li>
-                  <li><a href="login.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Login</a></li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> About us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Services
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Team
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Project
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Login
+                    </Link>
+                  </li>
                 </ul>
               </div>
               <div className="col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
                 <h5 className="footer-head">Departments</h5>
                 <ul className="list-unstyled footer-list mt-4">
-                  <li><a href="departments.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Eye Care</a></li>
-                  <li><a href="departments.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Psychotherapy</a></li>
-                  <li><a href="departments.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Dental Care</a></li>
-                  <li><a href="departments.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Orthopedic</a></li>
-                  <li><a href="departments.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Cardiology</a></li>
-                  <li><a href="departments.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Gynecology</a></li>
-                  <li><a href="departments.html" className="text-foot"><i className="mdi mdi-chevron-right me-1"></i> Neurology</a></li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Eye Care
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Psychotherapy
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Dental Care
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Orthopedic
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/" className="text-foot">
+                      <FiChevronRight /> Cardiology
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="departments.html" className="text-foot">
+                      <FiChevronRight /> Gynecology
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="departments.html" className="text-foot">
+                      <FiChevronRight /> Neurology
+                    </Link>
+                  </li>
                 </ul>
               </div>
               <div className="col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
                 <h5 className="footer-head">Contact us</h5>
                 <ul className="list-unstyled footer-list mt-4">
                   <li className="d-flex align-items-center">
-                    <a href="mailto:contact@example.com" className="text-foot ms-2">contact@example.com</a>
+                    <a
+                      href="mailto:contact@example.com"
+                      className="text-foot ms-2"
+                    >
+                     <BsEnvelope/> contact@example.com
+                    </a>
                   </li>
                   <li className="d-flex align-items-center">
-                    <a href="tel:+152534-468-854" className="text-foot ms-2">+152 534-468-854</a>
+                    <a href="tel:+152534-468-854" className="text-foot ms-2">
+                    <BsTelephone/> +152 534-468-854
+                    </a>
                   </li>
                   <li className="d-flex align-items-center">
-                    <a href="javascript:void(0)" className="video-play-icon text-foot ms-2">View on Google map</a>
+                    <a
+                      href="javascript:void(0)"
+                      className="video-play-icon text-foot ms-2"
+                    >
+                      <FiMapPin/> on Google map
+                    </a>
                   </li>
                 </ul>
                 <ul className="list-unstyled social-icon footer-social mb-0 mt-4">
-                  <li className="list-inline-item"><a href="#" className="rounded-pill"></a></li>
-                  <li className="list-inline-item"><a href="#" className="rounded-pill"></a></li>
-                  <li className="list-inline-item"><a href="#" className="rounded-pill"></a></li>
-                  <li className="list-inline-item"><a href="#" className="rounded-pill"></a></li>
+                  <li className="list-inline-item">
+                    <FiFacebook/>
+                  </li>
+                  <li className="list-inline-item">
+                    <FiInstagram/>
+                  </li>
+                  <li className="list-inline-item">
+                    <FiTwitter/>
+                  </li>
+                  <li className="list-inline-item">
+                    <FiLinkedin/>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -72,17 +151,45 @@ const Footer = () => {
                     src="https://www.youtube.com/s/player/bbe1b497/www-widgetapi.vflset/www-widgetapi.js"
                     async=""
                   ></script>
-                  <script id="iframe_api" src="https://www.youtube.com/iframe_api"></script>
-                  {new Date().getFullYear()} © Doctris. Design with <i className="mdi mdi-heart text-danger"></i> by <a href="https://shreethemes.in/" target="_blank" className="text-reset">Shreethemes</a>.
+                  <script
+                    id="iframe_api"
+                    src="https://www.youtube.com/iframe_api"
+                  ></script>
+                  {new Date().getFullYear()} © Doctris. Design with{" "}
+                  <FiHeart fill="red" color="red"/> by{" "}
+                  <Link
+                    href="https://shreethemes.in/"
+                    target="_blank"
+                    className="text-reset text-decoration-none"
+                  >
+                    Shreethemes
+                  </Link>
+                  .
                 </p>
               </div>
             </div>
             <div className="col-sm-6 mt-4 mt-sm-0">
               <ul className="list-unstyled footer-list text-sm-end text-center mb-0">
-                <li className="list-inline-item"><a href="terms.html" className="text-foot me-2">Terms</a></li>
-                <li className="list-inline-item"><a href="privacy.html" className="text-foot me-2">Privacy</a></li>
-                <li className="list-inline-item"><a href="aboutus.html" className="text-foot me-2">About</a></li>
-                <li className="list-inline-item"><a href="contact.html" className="text-foot me-2">Contact</a></li>
+                <li className="list-inline-item">
+                  <Link to="/" className="text-foot me-2">
+                    Terms
+                  </Link>
+                </li>
+                <li className="list-inline-item">
+                  <Link to="/" className="text-foot me-2">
+                    Privacy
+                  </Link>
+                </li>
+                <li className="list-inline-item">
+                  <Link to="/" className="text-foot me-2">
+                    About
+                  </Link>
+                </li>
+                <li className="list-inline-item">
+                  <Link to="/" className="text-foot me-2">
+                    Contact
+                  </Link>
+                </li>
               </ul>
             </div>
           </div>
