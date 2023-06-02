c71d98253d9d1a01459573735bcb8ebde2c5c5bc
deeppankaj <you@example.com>
2023-06-02 13:14:20 -0500

    office6

--- src/components/Home/FindDoctorSection.js
+++ src/components/Home/FindDoctorSection.js
@@ -11,10 +11,10 @@ const FindDoctorSection = () => {
               <h4 className="heading mb-3">Find Best Doctor</h4>
               <p className="para-desc text-muted mb-0">Great doctor if you need your family member to get immediate assistance, emergency treatment or a simple consultation.</p>
             </div>
-            <div className="search-form mt-4">
+            <div className="search-form mt-4 mx-2">
               <form className="ms-0" style={{ mixWidth: '550px' }}>
                 <div className="mb-2 border rounded-pill d-flex">
-                  <input type="text" id="name" name="name" className="rounded p-2 col-12" required placeholder="Doctor name..." />
+                  <input type="text" id="name" name="name" className="rounded-pill ml-2 border-0 col-12" required placeholder="    Doctor name..." />
                   <button type="submit" className="btn border flex gap-2 rounded-pill btn-primary py-2">
                    <BiSearch/> Search
                   </button>
